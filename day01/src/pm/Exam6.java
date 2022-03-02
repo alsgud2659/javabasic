8ec39389fcf57d8e41241d18ee8a7db7eb1898e2
alsgud2659 <94655599+alsgud2659@users.noreply.github.com>
2022-03-02 16:36:48 +0900

    .

--- day01/src/pm/Exam6.java
+++ day01/src/pm/Exam6.java
@@ -24,6 +24,9 @@ public class Exam6 {
 		// 형변환 type casting
 		// boolean을 제외한 기본 자료형은 서로 형을 변환할 수 있다.
 		// 타입 변환을 수행할 때는 저장할 수 있는 값의 범위나 종류가 달라지므로 값이 변화 할 수 있다.
+		// 타입 변환에는 자바가 자동으로 수행하는 '자동 타입변환'과 개발자가 직접 변환을 수행하는 '수동 타입변환'이 있다.
+		// 크기가 작은 자료형을 큰 자료형에 대입할 때는 손실이 발생하지 않는다.
+		// 따라서 타입변환을 개발자가 하지 않아도 자동으로 변환된다. ( 업캐스팅 up casting)
 		
 		int value11 = (int)5.3;
 		
@@ -41,7 +44,7 @@ public class Exam6 {
 		// boolean 을 제외한 기본 자료형은 서로 연산이 가능하다.
 		// 이때 모든 연산은 같은 자료형끼리만 가능하고 연산 결과 역시 같은 자료형이 나온다.
 		// 단, int 보다 작은 자료형 간의 연산 결과는 int가 나온다 ex) byte + byte short + short
-		// 이유는 cpu
+		// 이유는 cpu에서 연산 최소단위가 int 이므로 int보다 작은 자료형은 일단 int로 읽어와서 연산을 수행하기 때문이다.
 		
 		int value41 = 3 + 5;
 		int value42 = 8 / 5;
@@ -64,7 +67,7 @@ public class Exam6 {
 		double value50 = data13 + data14; // int를 double로 자동 형변환
 		
 		
-		System.out.println((double)(3/2));
+		
 
 	}
 	
